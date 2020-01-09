507ab0bb094f91e4f6ee8ff4b8531391697d9431
Marc Martinez <calored11@gmail.com>
2020-01-09 19:19:41 +0100

    primer commit

---
+++ android/src/com/mo8drop/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mo8drop.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mo8drop.game.MyMO8Drop;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyMO8Drop(), config);
+	}
+}
---
+++ desktop/src/com/mo8drop/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mo8drop.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mo8drop.game.MyMO8Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyMO8Drop(), config);
+	}
+}
